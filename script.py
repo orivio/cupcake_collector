c5d42fa7105ded7966e6042fa9c4ca8519d9e28e
Russlan Hossain <84582272+orivio@users.noreply.github.com>
2026-09-20 00:42:43 -0400

    added basic anim
    need to fix right anim

--- script.py
+++ script.py
@@ -10,6 +10,20 @@ class Player:
         self.idle = pygame.image.load(r"png sprites\paperclip idle.png").convert_alpha()
         self.idle = pygame.transform.scale(self.idle, (50, 50))
 
+        self.fall = pygame.image.load(r"png sprites\paperclip fall.png").convert_alpha()
+        self.fall = pygame.transform.scale(self.fall, (50, 50))
+
+        self.jump = pygame.image.load(r"png sprites\paperclip jump.png").convert_alpha()
+        self.jump = pygame.transform.scale(self.jump, (50, 50))
+
+        self.left = pygame.image.load(r"png sprites\paperclip left.png").convert_alpha()
+        self.left = pygame.transform.scale(self.left, (50, 50))
+
+        self.right = pygame.image.load(r"png sprites\paperclip right.png").convert_alpha()
+        self.right = pygame.transform.scale(self.right, (50, 50))
+
+        self.anim = self.idle
+
 gameloop = True
 WIDTH = 600
 HEIGHT = 400
@@ -56,11 +70,21 @@ while gameloop:
 
     if on_ground:
          grav_velocity = 0
+         if not keys[pygame.K_LEFT] or keys[pygame.K_a] or keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+              player.anim = player.idle
+         elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+              player.anim = player.right
+         elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+              player.anim = player.left
     else:
          grav_velocity = 1
+         if player.dy < 1:
+              player.anim = player.jump
+         else:
+              player.anim = player.fall
 
     screen.fill((84, 156, 105))
-    screen.blit(player.idle, (player.x, player.y))
+    screen.blit(player.anim, (player.x, player.y))
     
     clock.tick(60)
     pygame.display.flip()
